7b892a0fe871898f5723ce761cf035f6d2e6f042
Ivy <yvonne.richter86@gmx.de>
2021-12-16 11:29:09 +0000

    chain erstellen

--- blockchain.js
+++ blockchain.js
@@ -2,7 +2,16 @@ const Block = require("./block");
 
 class Blockchain{
 
+    constructor(){
+        this.chain = [Block.genesis()];
+    }
+    // Block.mineBlock(Block.genesisi(),"test");
 
+    addBlock(data){ // block erzeugen und an chain anhängen
+        const block = Block.mineBlock(this.chain[this.chain.lenght-1],data);
+        this.chain.push(block);// block an die Kette anhängen
+        return block; //monitoring
+    }
 
 
 
--- dev-test.js
+++ dev-test.js
@@ -1,9 +1,16 @@
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
 //const block = new Block("time","hash1","hash2","data");
 //console.log(block.toString());
 
 //console.log(Block.genesis().toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"test");
-console.log(testBlock.toString());
+//const testBlock = Block.mineBlock(Block.genesis(),"test");
+//console.log(testBlock.toString());
+
+const blockchain = new Blockchain();
+console.log(blockchain);
+
+blockchain.addBlock("100 € an boo, 50 $ von foo");
+console.log(blockchain);
